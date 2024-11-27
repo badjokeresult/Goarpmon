a7558f6f9eb63016447e8b7d5232898aea927de6
glomo <badjokeresult@gmail.com>
2024-11-27 16:50:22 +0300

    initial-commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module goarpmon
+
+go 1.22.0
+
+require github.com/gosnmp/gosnmp v1.38.0
---
+++ main.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"log"
+	"strings"
+
+	"github.com/gosnmp/gosnmp"
+)
+
+type SnmpData struct {
+	Data []SnmpEntry `json:"data"`
+}
+
+type SnmpEntry struct {
+	IpAddress string `json:"ipAddress"`
+	HwAddress string `json:"hwAddress"`
+}
+
+func main() {
+
+	results, err := getRawArpTableBySNMP("192.168.10.42", 161, "test-arp-table", "1.3.6.1.2.1.4.22.1.2")
+	if err != nil {
+		log.Fatalf("Error: %s", err)
+	}
+
+	parsed_table, _ := parseRawArpTableAsJson(results)
+
+	fmt.Println(parsed_table)
+
+	data, err := json.Marshal(parsed_table)
+	if err != nil {
+		log.Fatalf("Error: %s", err)
+	}
+
+}
+
+func getRawArpTableBySNMP(host string, port uint16, community string, oid string) ([]gosnmp.SnmpPDU, error) {
+	g := &gosnmp.GoSNMP{
+		Target:    host,
+		Port:      port,
+		Community: community,
+		Version:   gosnmp.Version2c,
+		Timeout:   gosnmp.Default.Timeout,
+	}
+
+	err := g.Connect()
+	if err != nil {
+		log.Fatalf("Error connecting")
+	}
+	defer g.Conn.Close()
+
+	results, err := g.BulkWalkAll(oid)
+	if err != nil {
+		log.Fatalf("Error getting ARP")
+	}
+
+	return results, nil
+}
+
+func parseRawArpTableAsJson(table []gosnmp.SnmpPDU) (*SnmpData, error) {
+	results := []SnmpEntry{}
+	for _, entry := range table {
+		ipAddr, _ := formatIpAddr(entry.Name)
+		value := hex.EncodeToString(entry.Value.([]byte))
+		hwAddr, _ := formatMacAddr(string(value))
+		results = append(results, SnmpEntry{
+			IpAddress: ipAddr,
+			HwAddress: hwAddr,
+		})
+	}
+
+	return &SnmpData{
+		Data: results,
+	}, nil
+}
+
+func formatIpAddr(addr string) (string, error) {
+	ipAddrArr := strings.Split(addr, ".")
+	ipAddr := strings.Join(ipAddrArr[len(ipAddrArr)-4:], ".")
+	return ipAddr, nil
+}
+
+func formatMacAddr(addr string) (string, error) {
+	tmpArr := []string{}
+	for i := 0; i < len(addr); i += 2 {
+		tmpArr = append(tmpArr, addr[i:i+2])
+	}
+	mac := strings.Join(tmpArr, ":")
+	return mac, nil
+}
