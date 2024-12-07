9225ddc516eddff59e0c312d8b68db5f5bcfb781
glomo <badjokeresult@gmail.com>
2024-12-07 18:49:05 +0300

    zabbix-sender-improvements

--- zabbix/main.go
+++ zabbix/main.go
@@ -2,27 +2,21 @@ package zabbix
 
 import (
 	"encoding/json"
-	"os"
+
+	"goarpmon/logger"
+
 	"os/exec"
 )
 
-func sendData(sender string, config string, key string, value interface{}) error {
+func SendData(logFilePath string, maxLogSizeMb int, sender string, config string, key string, value interface{}) error {
+	logger.SetLoggerPath(logFilePath, maxLogSizeMb)
+
 	data, err := json.Marshal(value)
 	if err != nil {
 		return err
 	}
 
 	cmd := exec.Command(sender, "-c", config, "-k", key, "-o", string(data[:]), "-vv")
-
-	currDir := os.Getenv("ARPMONDIR")
-	f, err := os.OpenFile(currDir+"/"+"zabbix_sender.log", os.O_RDWR|os.O_APPEND|os.O_CREATE, 0600)
-	if err != nil {
-		return err
-	}
-	defer f.Close()
-	cmd.Stdout = f
-	cmd.Stderr = f
-	cmd.Dir = currDir
 
 	err = cmd.Run()
 	return err
