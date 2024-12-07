b4918cc4eeb0043c3c92af9bc4a30fba0660b74c
glomo <badjokeresult@gmail.com>
2024-12-07 18:48:52 +0300

    logging-settings-improvements

--- logger/main.go
+++ logger/main.go
@@ -1,73 +1,34 @@
 package logger
 
 import (
-	"bytes"
-	"compress/gzip"
 	"log"
 	"os"
-	"strconv"
-	"strings"
+
+	"github.com/natefinch/lumberjack"
 )
 
-func StartLogging(logFilePath string, maxLogFileSize int64) error {
+func StartLogging(logFilePath string, maxLogFileSizeMb int) (*log.Logger, error) {
 	f, err := os.OpenFile(logFilePath, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0600)
 	if err != nil {
-		return err
+		return nil, err
 	}
 	defer f.Close()
 
-	fileStat, err := f.Stat()
-	if err != nil {
-		return err
-	}
-
-	if fileStat.Size() >= maxLogFileSize {
-		if err := rotateLogFile(f); err != nil {
-			return err
-		}
-	}
-
-	log.SetOutput(f)
-	return nil
+	log := log.New(f, "", log.Ldate|log.Ltime)
+	log.SetOutput(&lumberjack.Logger{
+		Filename:   logFilePath,
+		MaxSize:    maxLogFileSizeMb,
+		MaxBackups: 5,
+		MaxAge:     30,
+	})
+	return log, nil
 }
 
-func rotateLogFile(file *os.File) error {
-	filenames, err := os.ReadDir("./")
-	if err != nil {
-		return err
-	}
-
-	fileIndexMax := 0
-	for _, name := range filenames {
-		fileNameParts := strings.Split(name.Name(), ".")
-		fileNameIndex, err := strconv.Atoi(fileNameParts[len(fileNameParts)-2])
-		if err != nil {
-			return err
-		}
-		if fileNameIndex > fileIndexMax {
-			fileIndexMax = fileNameIndex
-		}
-	}
-
-	fileName := file.Name() + "." + strconv.Itoa(fileIndexMax) + ".gz"
-
-	err = compressWithGzip(file, fileName)
-	return err
-}
-
-func compressWithGzip(oldFile *os.File, newFileName string) error {
-	var b bytes.Buffer
-	content := []byte{}
-	w := gzip.NewWriter(&b)
-	if _, err := oldFile.Read(content); err != nil {
-		return err
-	}
-	w.Write(content)
-	w.Close()
-	if err := os.WriteFile(newFileName, content, 0600); err != nil {
-		return err
-	}
-
-	err := oldFile.Truncate(0)
-	return err
+func SetLoggerPath(logFilePath string, MaxFileSizeMb int) {
+	log.SetOutput(&lumberjack.Logger{
+		Filename:   logFilePath,
+		MaxSize:    MaxFileSizeMb,
+		MaxBackups: 5,
+		MaxAge:     30,
+	})
 }
