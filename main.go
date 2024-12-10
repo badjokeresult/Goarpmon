52317fd9d58d5b37c9daf1c570d1d60ab79fc8de
glomo <badjokeresult@gmail.com>
2024-12-10 16:17:56 +0300

    zabbix-refact

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"goarpmon/config"
 	"goarpmon/snmp"
 	"goarpmon/zabbix"
 	"log"
@@ -11,23 +12,31 @@ import (
 )
 
 func main() {
+	conf, err := config.Parse("/etc/arpmon/config.toml")
+	if err != nil {
+		log.Fatalf("Error: %s", err)
+	}
+	log.Println("Config file `/etc/arpmon/config.toml` was parsed successfully")
+
 	host := flag.String("host", "0.0.0.0", "Network device IPv4-address or hostname")
 	community := flag.String("community", "community", "SNMPv2c community name")
 	flag.Parse()
 
-	logFileName := "/var/log/arpmon/arpmon.log"
+	logFileName := conf.Arp.Log
 	f, err := os.OpenFile(logFileName, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0600)
 	if err != nil {
 		log.Fatalf("Error start logging: %s", err)
 	}
 	defer f.Close()
 	log.SetOutput(f)
+	log.Println("Logging was started successfully")
 
-	arpDbFile := "/etc/arpmon/arp.db"
+	arpDbFile := conf.Arp.Db
 	arpTable := new(snmp.ArpTable)
 	if err := arpTable.SetWithSnmpArpTableData(*host, 161, *community, arpDbFile); err != nil {
 		log.Fatalf("Error retrieving ARP table: %s", err)
 	}
+	log.Println("ARP table was retrieved successfully")
 
 	macIpsAddressesTable := new(snmp.AddressesTable)
 	ipMacsAddressesTable := new(snmp.AddressesTable)
@@ -35,34 +44,40 @@ func main() {
 	macIpsAddressesTable.CalcIpAddressesDissonances(arpTable)
 	ipMacsAddressesTable.CalcMacAddressesDissonances(arpTable)
 
-	sender := "/usr/bin/zabbix_sender"
-	config := "/etc/zabbix/zabbix_sender.conf"
+	sender := conf.Zabbix.Sender
+	config := conf.Zabbix.Config
+	zabbixLog := conf.Zabbix.Log
 	key := "arp.discovery"
-	if err := zabbix.SendData(sender, config, key, arpTable); err != nil {
+	if err := zabbix.SendData(sender, config, key, zabbixLog, arpTable); err != nil {
 		log.Fatalf("Error sending to Zabbix: %s", err)
 	}
+	log.Printf("`arp.discovery` was sent successfully. Data: %v", arpTable)
 	for _, entry := range macIpsAddressesTable.Data {
 		key = fmt.Sprintf("arp.macIps[%s]", entry.KeyAddr)
 		value := strings.Join(entry.ValueAddrs, " ")
-		if err := zabbix.SendData(sender, config, key, value); err != nil {
+		if err := zabbix.SendData(sender, config, key, zabbixLog, value); err != nil {
 			log.Fatalf("Error sending to Zabbix: %s", err)
 		}
+		log.Printf("`%s` was sent successfully. Data: %v", key, value)
 		key = fmt.Sprintf("arp.ipCount[%s]", entry.KeyAddr)
 		val := len(entry.ValueAddrs)
-		if err := zabbix.SendData(sender, config, key, val); err != nil {
+		if err := zabbix.SendData(sender, config, key, zabbixLog, val); err != nil {
 			log.Fatalf("Error sending to Zabbix: %s", err)
 		}
+		log.Printf("`%s` was sent successfully. Data: %v", key, val)
 	}
 	for _, entry := range ipMacsAddressesTable.Data {
 		key = fmt.Sprintf("arp.ipMacs[%s]", entry.KeyAddr)
 		value := strings.Join(entry.ValueAddrs, " ")
-		if err := zabbix.SendData(sender, config, key, value); err != nil {
+		if err := zabbix.SendData(sender, config, key, zabbixLog, value); err != nil {
 			log.Fatalf("Error sending to Zabbix: %s", err)
 		}
+		log.Printf("`%s` was sent successfully. Data: %v", key, value)
 		key = fmt.Sprintf("arp.macCount[%s]", entry.KeyAddr)
 		val := len(entry.ValueAddrs)
-		if err := zabbix.SendData(sender, config, key, val); err != nil {
+		if err := zabbix.SendData(sender, config, key, zabbixLog, val); err != nil {
 			log.Fatalf("Error sending to Zabbix: %s", err)
 		}
+		log.Printf("`%s` was sent successfully. Data: %v", key, val)
 	}
 }
--- zabbix/main.go
+++ zabbix/main.go
@@ -3,24 +3,25 @@ package zabbix
 import (
 	"encoding/json"
 	"os"
+	"os/exec"
 )
 
-func SendData(sender string, config string, key string, value interface{}) error {
-	// data, err := json.Marshal(value)
-	// if err != nil {
-	// 	return err
-	// }
-
-	// cmd := exec.Command(sender, "-c", config, "-k", key, "-o", string(data[:]), "-vv >/var/log/arpmon/zabbix_sender.log 2>&1")
-
-	// err = cmd.Run()
-	// return err
+func SendData(sender string, config string, key string, logFile string, value interface{}) error {
 	data, err := json.Marshal(value)
 	if err != nil {
 		return err
 	}
-	if _, err := os.Stdout.Write(data); err != nil {
+
+	cmd := exec.Command(sender, "-c", config, "-k", key, "-o", string(data[:]), "-vv")
+	f, err := os.OpenFile(logFile, os.O_RDWR, 0640)
+	if err != nil {
 		return err
 	}
-	return nil
+	defer f.Close()
+
+	cmd.Stdout = f
+	cmd.Stderr = f
+
+	err = cmd.Run()
+	return err
 }
